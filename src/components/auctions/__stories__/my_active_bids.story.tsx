8bcbdbb8eb42f5658a287c2b04bea4186209dc2b
Erik Stockmeier <erik.d.stock@gmail.com>
2017-10-09 12:55:33 -0400

    make something appear

--- src/components/auctions/__stories__/my_active_bids.story.tsx
+++ src/components/auctions/__stories__/my_active_bids.story.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 
 import MyActiveBids from "../my_active_bids"
 
-storiesOf("Components/Auctions/My Active Bids", module).add("Default", () => {
+storiesOf("Auctions/My Active Bids", module).add("Default", () => {
   return (
     <div>
       <MyActiveBids />
