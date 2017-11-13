5bef96ae7c0959f7826c5574a13b0476b2483f97
Erik Stockmeier <erik.d.stock@gmail.com>
2017-11-13 09:09:11 -0500

    import buttons and stuff

--- src/components/auctions/LotStatus.tsx
+++ src/components/auctions/LotStatus.tsx
@@ -1,3 +1,4 @@
+import { get } from 'lodash'
 import PropTypes from "prop-types"
 import React from "react"
 import Relay from "react-relay"
@@ -5,9 +6,36 @@ import styled from "styled-components"
 import colors from "../../Assets/Colors"
 import * as fonts from "../../Assets/Fonts"
 
+import Buttons from '../Buttons'
+const { Button, InvertedButton } = Buttons
+
 // const NarrowContainer = styled.div`
 //   min-width: 260px
 // `
+
+// Example lot standings query lot_standings{
+//   is_highest_bidder
+//   is_leading_bidder
+//   sale {
+//     name
+//     id
+//   }
+//   sale_artwork{
+//     artwork {
+//       title
+//     }
+//   }
+// }
+
+const LotStandingStatus: React.SFC<LotStandingProps> = (props) => {
+  return <div>{ props.isHighestBidder ? 'Winner' : 'Loser'}</div>
+}
+
+export interface LotStandingProps extends React.HTMLProps<any> {
+  isLeadingBidder: boolean
+  isHighestBidder: boolean // accounts for reserve
+  reserveStatus: string
+}
 
 export interface LotStatusProps extends React.HTMLProps<LotStatus> {
   title: string
@@ -17,29 +45,64 @@ export interface LotStatusProps extends React.HTMLProps<LotStatus> {
   displaySellingPrice: string
   displayHighBidAmount: string
   imgRef?: string
-  bid?: object
+  lotStanding?: LotStandingProps
 }
 
 
-const LotStatusAnchor = styled.a`
+const LotStatusContainer = styled.div`
   display: flex;
   width: 100%;
-  height: 77px;
-  border: 1px solid ${colors.grayMedium}
+  height: 92px;
+  border: 1px solid ${colors.grayMedium};
+  text-decoration: none;
 `
 
 const Image = styled.img`
-  flex: none
+  flex: none;
   width: 60px;
   height: 60px;
+  margin: 16px 12px 16px 16px;
 `
 
+const LotInfo = props =>
+  <div>{props.title}, {props.artistName} </div>
+
+const StyledLotInfo = styled(LotInfo)`
+  display: flex;
+  flex-direction: column;
+`
+
+const RightMatter = props => {
+  const {lotStanding, href} = props
+  let BidButton = Button
+  if (get(lotStanding, 'isLeadingBidder') === false) {
+    BidButton = InvertedButton
+  }
+  
+  // const CTA = props.bid ? <Button>With Bid!</Button> : <InvertedButton >No Bid </ InvertedButton>
+  return (
+    <div>
+      <div>{ lotStanding && 'bid' || 'nobid'}</div>
+      <BidButton {{href}}>Bid</BidButton>
+    </div>
+  )
+}
+
+const StyledRightMatter = styled(RightMatter)`
+  display: flex;
+  flex-direction: column;
+`
+
+// Should this be an anchor
 export class LotStatus extends React.Component<LotStatusProps, null> {
   render() {
+    const { imgRef, href, lotStanding, ...infoProps } = this.props
     return (
-      <LotStatusAnchor href={this.props.href} >
-        <Image  />
-      </LotStatusAnchor>
+      <LotStatusContainer href={href} >
+        <Image src={imgRef} />
+        <StyledLotInfo {...infoProps} />
+        <StyledRightMatter {{href, lotStanding}} />
+      </LotStatusContainer>
     )
   }
 }
--- src/components/auctions/__stories__/LotStatus.story.tsx
+++ src/components/auctions/__stories__/LotStatus.story.tsx
@@ -1,5 +1,6 @@
 import { storiesOf } from "@storybook/react"
 import * as React from "react"
+import styled from 'styled-components'
 
 import * as Artsy from "../../Components/Artsy"
 import { artsyNetworkLayer } from "../../Relay/config"
@@ -9,11 +10,14 @@ import { LotStatus, LotStatusProps } from "../LotStatus"
 //   Relay.injectNetworkLayer(props.user)
 // }
 
-storiesOf("Auctions/Lot Status", module).add("Default", () => {
+storiesOf("Auctions/Lot Status", module).add("300px", () => {
 
+  const Container = styled.div`
+    width: ${props => props.width}px;
+  `
   // see reaction/src/components/__stories__/ArtworkGrid.story.tsx
   return (
-    <div>
+    <Container width='300' >
       <LotStatus
         title="Big Painting"
         href="artsy.net/art/big-painting"
@@ -23,6 +27,6 @@ storiesOf("Auctions/Lot Status", module).add("Default", () => {
         displaySellingPrice="$48,000"
         displayHighBidAmount="$55,000"
       />
-    </div>
+    </Container>
   )
 })
